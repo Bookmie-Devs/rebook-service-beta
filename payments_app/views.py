84503ea270cf56f74aee24b0e2eebbafb4240392
Ethen <Ethenatx@gmail.com>
2023-10-07 11:16:47 +0000

    🪢error handlers in tenant_auth view

--- payments_app/views.py
+++ payments_app/views.py
@@ -126,72 +126,75 @@ def verify_payment(request, reference):
 def tenant_auth(request):
     try:
         tenant_id = Tenant.objects.get(user=request.user).tenant_id
+        get_tenant = Tenant.objects.get(tenant_id=tenant_id)
+
+        # room of tenant
+        room = get_tenant.room
+
+        #qrcode name for user after payments
+        qrcode_name = f'VerificationFiles/Ver_Qrcodes/{request.user.username} qrcode.png'
+
+        #tittle of pdf page to be generated
+        title = f"{request.user.username} Authentication details"
+
+        #subtittle of the pdf 
+        subtitle = 'www.GuudNyt.com'
+
+        """Fucntions to generate a list strings containing booking details for every user"""
+        booker_details= tenant_auth_details(user=request.user, 
+                                            room=room, tenant=get_tenant)
+
+        text_template = tenant_auth_message(user=request.user, 
+                                            room=room, tenant=get_tenant)
+
+
+        """CODES THAT GENERATE BOOKING PDF USING REPORT-LAB"""
+        buffer = BytesIO()
+        pdf = canvas.Canvas(buffer, pagesize=letter)
+
+        #Title of auth pdf
+        pdf.setTitle(title)
+        pdf.setFont("Helvetica", 20)
+        pdf.drawString(145, 730, title)
+
+        #Subtitle of auth pdf
+        pdf.setFont('Helvetica', 15)
+        pdf.drawString(250, 699, subtitle)
+
+        #tenant_details
+        details = pdf.beginText(290, 660)
+        details.setFont('Helvetica', 18)
+        details.setFillColor(colors.black)
+        ##############################
+        #looping over a list of strings
+        for detail in booker_details:
+            details.textLine(detail)
+        pdf.drawText(details)
+
+        #Main content of pdf
+        text = pdf.beginText(57, 440)
+        text.setFont('Helvetica', 18)
+        text.setFillColor(colors.black)
+        for line in text_template:
+            text.textLine(line)
+        pdf.drawText(text)
+
+        #generates qrcode for user
+        qr_code_image = generate_qrcode(get_tenant.tenant_id)
+        qr_code_image.save(qrcode_name)  # Save the QR code image
+        pdf.drawImage(qrcode_name, 50, 490, width=200, height=200)
+        pdf.showPage()
+        pdf.save()
+        buffer.seek(0)
+
+        #reponse to client with generated pdf
+        response = HttpResponse(content_type='application/pdf') 
+        response.write(buffer.getvalue())
+        return response
     
     except Tenant.DoesNotExist:
         messages.info(request, "You are not a tenant to get verification")
-        redirect('accounts:booking-and-payments')
+        return redirect('accounts:booking-and-payments')
 
-    get_tenant = Tenant.objects.get(tenant_id=tenant_id)
-    room = get_tenant.room
-
-    #qrcode name for user after payments
-    qrcode_name = f'VerificationFiles/Ver_Qrcodes/{request.user.username} qrcode.png'
-
-    #tittle of pdf page to be generated
-    title = f"{request.user.username} Authentication details"
-
-    #subtittle of the pdf 
-    subtitle = 'www.GuudNyt.com'
-
-    """Fucntions to generate a list strings containing booking details for every user"""
-    booker_details= tenant_auth_details(user=request.user, 
-                                        room=room, tenant=get_tenant)
-    
-    text_template = tenant_auth_message(user=request.user, 
-                                        room=room, tenant=get_tenant)
-    
-    
-    """CODES THAT GENERATE BOOKING PDF USING REPORT-LAB"""
-    buffer = BytesIO()
-    pdf = canvas.Canvas(buffer, pagesize=letter)
-
-    #Title of auth pdf
-    pdf.setTitle(title)
-    pdf.setFont("Helvetica", 20)
-    pdf.drawString(145, 730, title)
-
-    #Subtitle of auth pdf
-    pdf.setFont('Helvetica', 15)
-    pdf.drawString(250, 699, subtitle)
-
-    #tenant_details
-    details = pdf.beginText(290, 660)
-    details.setFont('Helvetica', 18)
-    details.setFillColor(colors.black)
-    ##############################
-    #looping over a list of strings
-    for detail in booker_details:
-        details.textLine(detail)
-    pdf.drawText(details)
-
-    #Main content of pdf
-    text = pdf.beginText(57, 440)
-    text.setFont('Helvetica', 18)
-    text.setFillColor(colors.black)
-    for line in text_template:
-        text.textLine(line)
-    pdf.drawText(text)
-
-    #generates qrcode for user
-    qr_code_image = generate_qrcode(get_tenant.tenant_id)
-    qr_code_image.save(qrcode_name)  # Save the QR code image
-    pdf.drawImage(qrcode_name, 50, 490, width=200, height=200)
-    pdf.showPage()
-    pdf.save()
-    buffer.seek(0)
-
-    #reponse to client with generated pdf
-    response = HttpResponse(content_type='application/pdf') 
-    response.write(buffer.getvalue())
-    return response
+  
 
