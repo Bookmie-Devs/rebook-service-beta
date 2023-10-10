6a6b30e9604e23866b375f4bf62a6af3ad66efc7
Ethen <Ethenatx@gmail.com>
2023-10-10 07:50:48 +0000

    💡customize room admin-panel

--- rooms_app/admin.py
+++ rooms_app/admin.py
@@ -2,4 +2,20 @@ from django.contrib import admin
 from .models import  RoomProfile
 # Register your models here.
 
-admin.site.register(RoomProfile)
+
+class CustomRoomAdminPanel(admin.ModelAdmin):
+
+    fieldsets = (
+        ('General', {
+            "fields": ('room_no', 'floor_no','room_price','hostel','campus',
+                       'room_capacity','room_img', 'rating'),}),
+        
+        ('Specs', {'fields':('contains_kitchen','shared_kitchen',
+                             'contains_bathroom','air_condition')}),
+
+        ('Status', {'fields':('booking_occupied', 'occupied')}),
+    )
+    
+
+
+admin.site.register(RoomProfile, CustomRoomAdminPanel)
