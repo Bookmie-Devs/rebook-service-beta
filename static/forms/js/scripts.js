a9c2183ecd6f1e7fab157dd23db155dbfbe718b3
LhilEthen <Ethenatx@gmail.com>
2023-11-17 23:49:48 +0000

    added section tags

--- static/forms/js/scripts.js
+++ static/forms/js/scripts.js
@@ -13,7 +13,6 @@ try {
     document.querySelectorAll('.section').forEach((section) => {
       section.style.display = 'none';
     });
-
     // Show the selected section
     document.getElementById(`section${sectionNumber}`).style.display = 'block';
   }
