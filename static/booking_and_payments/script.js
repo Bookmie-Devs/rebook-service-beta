fa766f10cb9110daaee74badbb37e040e76ba29b
LhilEthen <Ethenatx@gmail.com>
2023-11-21 14:02:13 +0000

    seperated map statics and added loader to profile

--- static/booking_and_payments/script.js
+++ static/booking_and_payments/script.js
@@ -2,10 +2,26 @@ const direction = document.getElementById('direction');
 const directionContainer = document.querySelector('.direction');
 const closebtn = document.querySelector('.close');
 
-direction.addEventListener('click', (e) => {
-    e.preventDefault();
-    directionContainer.style.display = 'block';
-})
+// loader
+const Load = document.querySelector('.load');
+
+// direction.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     directionContainer.style.display = 'block';
+// })
+
+
+direction.onclick =() => {
+
+    Load.style.display = "flex";
+
+    setTimeout(function () {
+
+    // Hide the loader after the delay
+    Load.style.display = 'none';
+
+    }, 6000);
+}
 
 closebtn.addEventListener('click', () => {
     directionContainer.style.display = 'none';
---
+++ static/maps/map.js
@@ -0,0 +1,31 @@
+// Initialize and add the map
+let map;
+
+async function initMap() {
+
+  // The location of hostel
+  const position = { lat: parseFloat(document.getElementById('lat').value), 
+                     lng: parseFloat(document.getElementById('lng').value) };
+
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at hostel locaiton
+  map = new Map(document.getElementById("map"), {
+    zoom: 18,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    // the hostel name as title
+    title:document.getElementById('hostel-name').value,
+  });
+}
+
+initMap();
