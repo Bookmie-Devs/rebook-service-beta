6ad6c5660f1f7686bf63dc91315f06e9f40c76f4
LhilEthen <Ethenatx@gmail.com>
2023-12-08 22:06:24 +0000

    map_direction cach

--- maps_app/urls.py
+++ maps_app/urls.py
@@ -4,6 +4,6 @@ from django.views.decorators.cache import cache_page
 app_name ="maps"
 
 urlpatterns = [
-    path('hostel-direction/<str:hostel_id>/', views.hostel_direction, name="hostel-direction"),
+    path('hostel-direction/<str:hostel_id>/',cache_page(60 * 15)(views.hostel_direction), name="hostel-direction"),
     path('all-hostels/',cache_page(60 * 15)(views.map_views), name='map-views')
     ]
