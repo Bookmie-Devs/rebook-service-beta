f44b05be2dcd4cf6b3484772ddb2d589bd6f7bee
LhilEthen <Ethenatx@gmail.com>
2024-01-03 06:58:29 +0000

    fixed error in gunicorn file

--- config/gunicorn/gunicorn.dev.py
+++ config/gunicorn/gunicorn.dev.py
@@ -1,7 +1,7 @@
 """Gunicorn *development* config file"""
 
 # Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
-wsgi_app = "confg.wsgi:application"
+wsgi_app = "config.wsgi:application"
 
 # The granularity of Error log outputs
 loglevel = "debug"
