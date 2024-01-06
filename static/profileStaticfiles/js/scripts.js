5fa3da5f4af71df187887af42786f8502afc21d7
3Roland59 <amarteyroland359@gmial.com>
2024-01-06 03:07:45 +0000

    multiple imgs for room_profile

--- static/profileStaticfiles/js/scripts.js
+++ static/profileStaticfiles/js/scripts.js
@@ -1,8 +1,11 @@
 
 // Variables
 
-let image = document.querySelector('.img').src;
-let url = "url('" + image + "')";
+let images = document.querySelectorAll('.img');
+
+images.forEach(img => {
+ if(img.parentElement.classList.contains('active')){
+let url = "url('" + img.src + "')";
 let bkg = document.querySelector('.section');
 
 // Dynamic Background
@@ -11,6 +14,47 @@ bkg.style.background = 'linear-gradient(rgba(0, 0, 0, 0.486), rgba(0, 0, 0, 0.80
 bkg.style.backgroundPosition = 'center';
 bkg.style.backgroundRepeat = 'no-repeat';
 bkg.style.backgroundSize = 'cover';
+ }
+});
+
+let currentIndex = 0;  // Initialize with the first image
+
+function checkBgf() {
+  images.forEach((img, index) => {
+    if (img.parentElement.classList.contains('active')) {
+      currentIndex = index;  // Update the current index
+      let nextIndex = (currentIndex + 1) % images.length;  // Calculate the next index
+
+      let url = "url('" + images[nextIndex].src + "')";
+      let bkg = document.querySelector('.section');
+
+      // Dynamic Background
+      bkg.style.background = 'linear-gradient(rgba(0, 0, 0, 0.486), rgba(0, 0, 0, 0.801))' + ',' + url;
+      bkg.style.backgroundPosition = 'center';
+      bkg.style.backgroundRepeat = 'no-repeat';
+      bkg.style.backgroundSize = 'cover';
+    }
+  });
+}
+
+function checkBgb() {
+  images.forEach((img, index) => {
+    if (img.parentElement.classList.contains('active')) {
+      currentIndex = index;  // Update the current index
+      let prevIndex = (currentIndex - 1) % images.length;  // Calculate the prev index
+
+      let url = "url('" + images[prevIndex].src + "')";
+      let bkg = document.querySelector('.section');
+
+      // Dynamic Background
+      bkg.style.background = 'linear-gradient(rgba(0, 0, 0, 0.486), rgba(0, 0, 0, 0.801))' + ',' + url;
+      bkg.style.backgroundPosition = 'center';
+      bkg.style.backgroundRepeat = 'no-repeat';
+      bkg.style.backgroundSize = 'cover';
+    }
+  });
+}
+
 
 function openNav() {
     document.getElementById("menu-btn").classList.toggle("menu_btn-style")
