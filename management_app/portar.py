5373eafea7e9069a7d332f7f6a7b2aff4e8415db
LhilEthen <Ethenatx@gmail.com>
2024-01-23 20:56:46 +0000

    reviews system => htmx

--- management_app/portar.py
+++ management_app/portar.py
@@ -1,13 +1,48 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponseForbidden
+from django.shortcuts import render
 from .models import Worker
 from core.models import Tenant
+from rooms_app.models import RoomProfile
 from django.utils import timezone
 from hostel_app.models import HostelProfile
 
+def portar_office(request: HttpRequest):
+    try:
+        portar = Worker.objects.get(user=request.user)
+        hostel = portar.hostel
+        context={'user':request.user, 'hostel':hostel}
+        return render(request, "portar_office.html",context)
+    except Worker.DoesNotExist:
+        return HttpResponseForbidden("You do not have permission to access this page")
+
 def tenants(request: HttpRequest):
-    portar = Worker.objects.get(user=request.user)
-    hostel = HostelProfile.objects.get(hostel=portar.hostel)
-    all_active_tenants = Tenant.objects.filter(hostel=hostel, end_date__lt=timezone.now())
+    try:
+        portar = Worker.objects.get(user=request.user)
+        hostel = portar.hostel
+        all_active_tenants = Tenant.objects.filter(hostel=hostel, end_date__gt=timezone.now()).all()
+        context={'tenants':all_active_tenants,'user':request.user}
+        return render(request, "portar_htmx/",context)
+    except Worker.DoesNotExist:
+        return HttpResponseForbidden("You do not have permission to access this page")
+    
+def rooms(request: HttpRequest):
+    try:
+        portar = Worker.objects.get(user=request.user)
+        hostel = portar.hostel
+        rooms = RoomProfile.objects.filter(hostel=hostel).all()
+        context={'rooms':rooms, 'user':request.user}
+        return render(request, "portar_htmx/",context)
+    except Worker.DoesNotExist:
+        return HttpResponseForbidden("You do not have permission to access this page")
+
+def profile(request: HttpRequest):
+    try:
+        portar = Worker.objects.get(user=request.user)
+        hostel = portar.hostel
+        context={'rooms':rooms, 'user':request.user}
+        return render(request, "portar_htmx/",context)
+    except Worker.DoesNotExist:
+        return HttpResponseForbidden("You do not have permission to access this page")
 
 
 
--- static/home/js/script.js
+++ static/home/js/script.js
@@ -80,139 +80,139 @@ try {
 THE OTHER ONSUBMIT EVENT LISTERNERS FROM CRASHING */
 
 
-// catch error is element is not found
-try {
-  // FEEDBACK/ISSUES FORM
-feedbackForm.onsubmit = () => {
-  
-  let username = document.getElementById('username').value
-  let useremail = document.getElementById('useremail').value
-  let userphone = document.getElementById('userphone').value
-  let message = document.getElementById('message').value
-  
-  feedbackButton.innerHTML = "Sending..."
-
-  fetch("/reviews/feedback-and-issues/", {
-   method: "POST",
-   body: JSON.stringify({
-  "name": username,
-  "email": useremail,
-  "phone": userphone,
-  "message":message,
-}),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8"
-  }
-})
-.then((response) => response.json())
-  .then((json) =>feedbackButton.innerHTML=json.message);
-
-  document.getElementById('username').value = ''
-  document.getElementById('useremail').value = ''
-  document.getElementById('userphone').value = ''
-  document.getElementById('message').value = ''
-
-  return false
-}
-} catch (error) {
-  
-}
-
-
-// CUSTOMER CARE FORM(CONTACT US)
-try {
-
-contactForm.onsubmit = () => {
-  let username = document.getElementById('username').value
-  let useremail = document.getElementById('useremail').value
-  let userphone = document.getElementById('userphone').value
-  let message = document.getElementById('message').value
-
-  contactButton.innerHTML = "Sending..."
-
-  fetch("/reviews/customer-care/", {
-   method: "POST",
-   body: JSON.stringify({
-  "name": username,
-  "email": useremail,
-  "phone": userphone,
-  "message":message,
-}),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8"
-  }
-})
-.then((response) => response.json())
-    .then((json) => contactButton.innerHTML = json.message);
-
-  document.getElementById('username').value = ''
-  document.getElementById('useremail').value = ''
-  document.getElementById('userphone').value = ''
-  document.getElementById('message').value = ''
-
-  return false}
-} catch (error) {
-  
-}
-
-
+// // catch error is element is not found
 // try {
-
-// quickSearch.onsubmit = () => {
-
-//     let campus = document.getElementById('campus').value
-//     let room_price_min = document.getElementById('room-price-min').value
-//     let room_price_max = document.getElementById('room-price-max').value
-//     let roomCapacity = document.getElementById('room-capacity').value
-//     let button = document.getElementById('quick-search-button')
-    
-//     // if string is empty for campus means its not regitered
-//     if (campus == ""){
-//       button.innerHTML = "unavailable on this campus yet";
-//       return false;
-//     }
+//   // FEEDBACK/ISSUES FORM
+// feedbackForm.onsubmit = () => {
   
-// fetch(`/rooms/campus-rooms/${campus}?room_capacity=${roomCapacity}&room_price_min=${room_price_min}&room_price_max=${room_price_max}`, {
-//      method: "GET",
-//   }).then(function(response){
-//     console.log(response)
-//     button.innerHTML = "Searching...";
-//     // get url from the response object and redirect user to that
-//     window.location.href = response.url;
-//   })
+//   let username = document.getElementById('username').value
+//   let useremail = document.getElementById('useremail').value
+//   let userphone = document.getElementById('userphone').value
+//   let message = document.getElementById('message').value
+  
+//   feedbackButton.innerHTML = "Sending..."
 
-//   return false;
+//   fetch("/reviews/feedback-and-issues/", {
+//    method: "POST",
+//    body: JSON.stringify({
+//   "name": username,
+//   "email": useremail,
+//   "phone": userphone,
+//   "message":message,
+// }),
+//   headers: {
+//     "Content-type": "application/json; charset=UTF-8"
+//   }
+// })
+// .then((response) => response.json())
+//   .then((json) =>feedbackButton.innerHTML=json.message);
+
+//   document.getElementById('username').value = ''
+//   document.getElementById('useremail').value = ''
+//   document.getElementById('userphone').value = ''
+//   document.getElementById('message').value = ''
+
+//   return false
 // }
-
 // } catch (error) {
   
 // }
 
-try {
+
+// // CUSTOMER CARE FORM(CONTACT US)
+// try {
+
+// contactForm.onsubmit = () => {
+//   let username = document.getElementById('username').value
+//   let useremail = document.getElementById('useremail').value
+//   let userphone = document.getElementById('userphone').value
+//   let message = document.getElementById('message').value
+
+//   contactButton.innerHTML = "Sending..."
+
+//   fetch("/reviews/customer-care/", {
+//    method: "POST",
+//    body: JSON.stringify({
+//   "name": username,
+//   "email": useremail,
+//   "phone": userphone,
+//   "message":message,
+// }),
+//   headers: {
+//     "Content-type": "application/json; charset=UTF-8"
+//   }
+// })
+// .then((response) => response.json())
+//     .then((json) => contactButton.innerHTML = json.message);
+
+//   document.getElementById('username').value = ''
+//   document.getElementById('useremail').value = ''
+//   document.getElementById('userphone').value = ''
+//   document.getElementById('message').value = ''
+
+//   return false}
+// } catch (error) {
   
-newsLetter.onsubmit = () =>{
-
-  let newsEmail = document.getElementById('news-email').value
-
-  subscribeButton.innerHTML = "Sending..."
-
-  fetch("/news-letter/", {
-   method: "POST",
-   body: JSON.stringify({
-  "email": newsEmail,
-}),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8"
-  }
-})
-.then((response) => response.json())
-    .then((json) => subscribeButton.innerHTML = json.message);
-
-  document.getElementById('news-email').value = ''
+// }
 
 
-  return false
-}
-} catch (error) {
+// // try {
+
+// // quickSearch.onsubmit = () => {
+
+// //     let campus = document.getElementById('campus').value
+// //     let room_price_min = document.getElementById('room-price-min').value
+// //     let room_price_max = document.getElementById('room-price-max').value
+// //     let roomCapacity = document.getElementById('room-capacity').value
+// //     let button = document.getElementById('quick-search-button')
+    
+// //     // if string is empty for campus means its not regitered
+// //     if (campus == ""){
+// //       button.innerHTML = "unavailable on this campus yet";
+// //       return false;
+// //     }
   
-}
+// // fetch(`/rooms/campus-rooms/${campus}?room_capacity=${roomCapacity}&room_price_min=${room_price_min}&room_price_max=${room_price_max}`, {
+// //      method: "GET",
+// //   }).then(function(response){
+// //     console.log(response)
+// //     button.innerHTML = "Searching...";
+// //     // get url from the response object and redirect user to that
+// //     window.location.href = response.url;
+// //   })
+
+// //   return false;
+// // }
+
+// // } catch (error) {
+  
+// // }
+
+// try {
+  
+// newsLetter.onsubmit = () =>{
+
+//   let newsEmail = document.getElementById('news-email').value
+
+//   subscribeButton.innerHTML = "Sending..."
+
+//   fetch("/news-letter/", {
+//    method: "POST",
+//    body: JSON.stringify({
+//   "email": newsEmail,
+// }),
+//   headers: {
+//     "Content-type": "application/json; charset=UTF-8"
+//   }
+// })
+// .then((response) => response.json())
+//     .then((json) => subscribeButton.innerHTML = json.message);
+
+//   document.getElementById('news-email').value = ''
+
+
+//   return false
+// }
+// } catch (error) {
+  
+// }
