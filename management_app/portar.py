a685b923213acd60af28b34fb7016b8dd0b8d92b
LhilEthen <Ethenatx@gmail.com>
2024-01-26 20:11:13 +0000

    added 0006_remove_worker_id_worker_worker_code_worker_worker_id

--- management_app/portar.py
+++ management_app/portar.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.http import HttpRequest, HttpResponseForbidden
 from django.shortcuts import render
 from .models import Worker
@@ -45,5 +46,44 @@ def profile(request: HttpRequest):
         return HttpResponseForbidden("You do not have permission to access this page")
 
 
+def update_room_price(self, request:HttpRequest, *args, **kwargs):
+    try:
+        room_capacity = request.POST.get('room_capacity') #room with capacity
+        new_price = request.POST.get('new_price') #price for those rooms
+        hostel = HostelProfile.objects.get(hostel_manager=request.user)
+        # check if room exist
+        if RoomProfile.objects.filter(hostel=hostel, room_capacity=room_capacity).exists():
+            rooms = RoomProfile.objects.filter(hostel=hostel, room_capacity=room_capacity)
+            # calculate ptf_pricing 
+            addtional_pricing: float = float(new_price) * settings.SUPPLY_COST_PERCENTAGE
+            new_ptf_room_price = float(new_price) + float(addtional_pricing)
 
+            rooms.update(room_price=new_price, ptf_room_price=new_ptf_room_price)
+            return render({'message':'Rooms price have been updated'}, status=status.HTTP_201_CREATED)
+        else: 
+            return render({'message':'Rooms does not exist'}, status=status.HTTP_404_NOT_FOUND)
+    except Worker.DoesNotExist:
+        return HttpResponseForbidden("You do not have permission to access this page")
+
+
+
+
+def edit_room(request: HttpRequest, room_id):
+    context = {"message":None}
+    try:
+        portar = Worker.objects.get(user=request.user)
+        room = RoomProfile.objects.get(room_id=room_id, hostel=portar.hostel)
+        if request.method=="POST":
+            price = float(request.POST.get('price'))
+            number_of_bed_spaces = int(request.POST.get('bed_space'))
+            occupied = request.POST.get('occupied')
+            room_number = request.POST.get('room_number')
+            if RoomProfile.objects.filter(room_no=room_number).exists():
+                context['message']="Room number already exist"
+                return render(request, 'portar/htmx_message.html',context)
+            context.update({"room":room})
+        return render(request, 'portar/htmx_room_form.html',context)
+
+    except Worker.DoesNotExist:
+        return HttpResponseForbidden("You do not have permission to access this page")
 
