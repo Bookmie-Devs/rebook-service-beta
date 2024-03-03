af6b9726c38848df035cc7dd64c66ee7f60c0a21
LhilEthen <Ethenatx@gmail.com>
2024-03-03 15:51:09 +0000

    changes

--- agents_app/admin.py
+++ agents_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Agent
+from .models import Agent, AgentSale
 # Register your models here.
 
 
@@ -16,6 +16,9 @@ class AgentAdmin(admin.ModelAdmin):
     list_display = ('user', 'agent_code' ,'is_verified','is_active','date_join',)
 
 
+class AgentSalesAdmin(admin.ModelAdmin):
+    list_display = ('agent', 'year', 'number_of_sales','date',)
 
 
+admin.site.register(AgentSale, AgentSalesAdmin)
 admin.site.register(Agent, AgentAdmin)
